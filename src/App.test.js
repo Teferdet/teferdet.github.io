d9dd990746a53fb8f4951ef525a2fc91a748bc16
George Teferdet <103109561+Teferdet@users.noreply.github.com>
2024-07-01 18:48:31 +0300

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js';
 
-test('renders learn react link', () => {
+test('renders George Teferdet', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/George Teferdet/i);
+  expect(textElement).toBeInTheDocument();
 });
