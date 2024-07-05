f6890a335d33d36b1995b6ac7a946a6122751a31
George Teferdet <103109561+Teferdet@users.noreply.github.com>
2024-07-05 23:34:13 +0300

    Add 404 Error handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import {useEffect} from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './index.css';
 import Home from './pages/home';
@@ -6,20 +5,14 @@ import Poems from './pages/poems';
 import Projects from "./pages/projects";
 
 function App() {
-    useEffect(() => {
-        const baseTag = document.createElement('base');
-        baseTag.href = '/me';
-        document.head.appendChild(baseTag);
-    }, []);
 
     // <Route path="me/projects" element={<Projects />} /> - in dev
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="me/poems" element={<Poems />} />
-
-                <Route path="me/*" element={<Home />} />
+                <Route path="poems" element={<Poems />} />
+                <Route path="*" element={<Home />} />
             </Routes>
         </BrowserRouter>
     );
